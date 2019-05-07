b88a86bbffd8fdc3dde8e074a9195358b892182e
Andy.Li <liyang@ushareit.com>
2019-05-07 11:02:51 +0800

    refactor some codes

--- WKURLSchemeHandler_Demo/MMURLSchemeHandler.m
+++ WKURLSchemeHandler_Demo/MMURLSchemeHandler.m
@@ -15,37 +15,45 @@ NSString * const customMTTPS = @"mttps";
 
 @implementation MMURLSchemeHandler
 {
-    NSMutableArray<id <WKURLSchemeTask>> *_urlSchemeTasks;
+    NSMutableArray<id<WKURLSchemeTask>> *_urlSchemeTasks;
 }
 
-static bool isLiyang(char ch)
+static bool isAllowedChar(char ch)
 {
     return (ch == '_' || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
 }
 
-static void replaceText(char* text) {
+static void replaceText(char *text)
+{
     int32_t http = 0;
     memcpy(&http, "http", sizeof(http));
-    uint32_t test = 0;
+    uint32_t tempText = 0;
     size_t len = strlen(text);
-    if (len >= 4) {
-        memcpy(&test, text, sizeof(test));
+    if (len >= 4)
+    {
+        memcpy(&tempText, text, sizeof(tempText));
         for (size_t i = 3; i < len; i++) {
-            if (test == http) {
-                if (i == 3 || !isLiyang(text[i-4])) {
-                    if ((i == len-1 && text[i+1] == ':') || !isLiyang(text[i+2])) {
+            if (tempText == http)
+            {
+                if (i == 3 || !isAllowedChar(text[i-4]))
+                {
+                    if ((i == len-1 && text[i+1] == ':') || !isAllowedChar(text[i+2]))
+                    {
+                        // http:
                         text[i-3] = 'm';
-                        printf("iiii %d\n", i);
-                        // http
-                    } else if (i < len-1 && text[i+1] == 's' && text[i+2] == ':' && (i == len-3 || !isLiyang(text[i+3]))) {
+                        printf("---->  %zu\n", i);
+                        
+                    }
+                    else if (i < len-1 && text[i+1] == 's' && text[i+2] == ':' && (i == len-3 || !isAllowedChar(text[i+3])))
+                    {
+                        // https:
                         text[i-3] = 'm';
-                        // https
-                        printf("iiii %d\n", i);
+                        printf("---->  %zu\n", i);
                     }
                 }
             }
-            test >>= 8;
-            test |= (text[i+1]<<24);
+            tempText >>= 8;
+            tempText |= (text[i+1]<<24);
         }
     }
 }
@@ -57,26 +65,33 @@ static void replaceText(char* text) {
     NSMutableURLRequest *request = [urlSchemeTask.request mutableCopy];
     
     NSURLComponents *components = [NSURLComponents componentsWithURL:request.URL resolvingAgainstBaseURL:NO];
-    if ([components.scheme isEqualToString:customMTTP]) {
+    if ([components.scheme isEqualToString:customMTTP])
+    {
         components.scheme = @"http";
-    } else if ([components.scheme isEqualToString:customMTTPS]) {
+    }
+    else if ([components.scheme isEqualToString:customMTTPS])
+    {
         components.scheme = @"https";
     }
     request.URL = components.URL;
-    [request addValue:@"1" forHTTPHeaderField:@"mttp"];
+    [request addValue:@"1" forHTTPHeaderField:@"mttp"]; // 抓包标记，只要带有这个标记的都表示被拦截
     
     [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         
-        if ([_urlSchemeTasks containsObject:urlSchemeTask]) {
-            if (error) {
+        if ([self->_urlSchemeTasks containsObject:urlSchemeTask])
+        {
+            if (error)
+            {
                 [urlSchemeTask didFailWithError:error];
-            } else {
-                if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            }
+            else
+            {
+                if ([response isKindOfClass:[NSHTTPURLResponse class]])
+                {
                     NSString *contentType = [(NSHTTPURLResponse *)response allHeaderFields][@"content-type"];
-                    if ([contentType hasPrefix:@"text/html"] ||
-                        [contentType hasPrefix:@"application/javascript"] ||
-                        [contentType hasPrefix:@"text/css"]) {
-                        printf("iiii  %s\n", request.URL.absoluteString.UTF8String);
+                    if ([contentType hasPrefix:@"text/html"] || [contentType hasPrefix:@"application/javascript"] || [contentType hasPrefix:@"text/css"])
+                    {
+                        printf("---->  %s\n", request.URL.absoluteString.UTF8String);
                         char end = 0;
                         NSMutableData *temp = [data mutableCopy];
                         [temp appendBytes:&end length:sizeof(end)];
@@ -86,7 +101,7 @@ static void replaceText(char* text) {
                         data = temp;
                         
                         [data writeToFile:[NSString stringWithFormat:@"/Users/liyang/Desktop/ithome/%@.txt", [self MD5WithStr:request.URL.absoluteString]]  atomically:YES];
-                        printf("%s\n", [self MD5WithStr:request.URL.absoluteString].UTF8String);
+                        printf("---->  %s\n", [self MD5WithStr:request.URL.absoluteString].UTF8String);
                         
                         printf("\n\n\n\n\n\n\n");
                     }
@@ -96,13 +111,13 @@ static void replaceText(char* text) {
                     [urlSchemeTask didReceiveResponse:response];
                     
                     [urlSchemeTask didReceiveData:data];
-                    
-                    [urlSchemeTask didFinish];
                 } @catch (NSException *exception) {
+                    [urlSchemeTask didFailWithError:error];
                 } @finally {
+                    [urlSchemeTask didFinish];
                 }
             }
-            [_urlSchemeTasks removeObjectIdenticalTo:urlSchemeTask];
+            [self->_urlSchemeTasks removeObjectIdenticalTo:urlSchemeTask];
         }
         
     }] resume];
